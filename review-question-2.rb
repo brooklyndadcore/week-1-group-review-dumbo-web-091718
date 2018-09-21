139718b3dc7593993615c65d2642d07144b39b22
brooklyndadcore <scott.waring@gmail.com>
2018-09-21 13:05:18 -0400

    finished questions review 2

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,8 +1,15 @@
 # Finish the implementation of the Car class so it has the functionality described below
 
 class Car
+  attr_reader :make, :model
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
 
-
+  def drive
+    puts "VROOOOOOOOOOOOM!"
+  end
 
 end
 
@@ -10,22 +17,23 @@ volvo_lightning = Car.new("Volvo", "Lightning")
 yugo = Car.new("Zastava", "Yugo")
 lada = Car.new("AvtoVAZ", "Lada")
 
-volvo_lightning.make
-#=> "Volvo"
-volvo_lightning.model
-#=> "Lightning"
+# volvo_lightning.make
+# #=> "Volvo"
+# volvo_lightning.model
+# #=> "Lightning"
+#
+# car.drive
+# # => "VROOOOOOOOOOOOM!"
 
-car.drive
-# => "VROOOOOOOOOOOOM!"
-
-Car.all
+# Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+# BONUS:
 
-volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
+volvo_lightning = Car.new( "Volvo", "Lightning")
 
-volvo_lightning.make
+p volvo_lightning.make
 #=> "Volvo"
-volvo_lightning.model
+p volvo_lightning.model
 #=> "Lightning"
+yugo.drive
