ccf83242dc45c38f937906a6ae231afe078ff2cf
brooklyndadcore <scott.waring@gmail.com>
2018-09-21 11:53:51 -0400

    completed questions

--- review-question-1.rb
+++ review-question-1.rb
@@ -59,11 +59,47 @@ pokemon = [
   ]
 }
 ]
-
-
 # How would you get the url for Bulbasaur's ability?
+# pokemon.each do |poke|
+#   if poke[:name] == "bulbasaur"
+#     p poke[:abilities][0][:ability][:url]
+#   end
+# end
+
 # How would you return the first pokemon with base experience over 40?
+
+# def find_first_poke(array)
+#   just_first_over_40 = array.select { |poke| poke[:base_experience] > 40 }
+# p just_first_over_40[0][:name]
+# end
+#
+# find_first_poke(pokemon)
+
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+# def find_all_poke(array)
+#   over_40 = array.select { |poke| poke[:base_experience] > 40 }
+#  p over_40
+# end
+#
+# find_all_poke(pokemon)
 # How would you return an array of all of the pokemon's names?
+# def poke_names(array)
+#   names_arr = []
+#   array.each {|poke| names_arr << poke[:name]}
+#   p names_arr
+# end
+#
+# poke_names(pokemon)
+
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
-#  whatever method you use should return true if there are any such pokemon, false if not.
+# def find_fatties(array)
+#   array.each do |poke|
+#     if poke[:weight] > 60
+#       return true
+#     end
+#   end
+#   return false
+# end
+#
+# p find_fatties(pokemon)
+ # whatever method you use should return true if there are any such pokemon, false if not.
